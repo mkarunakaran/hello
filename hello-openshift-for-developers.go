6f24a0e6ca413550dd02c24b05fb192abf441bc6
Murali <murali.karunakaran@gmail.com>
2025-04-24 10:09:09 -0400

    Changed Text

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello Murali!"
+		response = "Hello Murali For Openshift!"
 	}
 
 	fmt.Fprintln(w, response)
